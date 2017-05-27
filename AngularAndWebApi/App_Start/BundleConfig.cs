938f3c7f382804d27f5d34e5002ef3d06eb67bda
panververis <panververis@gmail.com>
2017-05-27 20:30:54 +0300

    Added Font-Awesome. Made progress with the basic Vehicles ListView (mid - dev)

--- AngularAndWebApi/App_Start/BundleConfig.cs
+++ AngularAndWebApi/App_Start/BundleConfig.cs
@@ -26,7 +26,8 @@ namespace AngularAndWebApi
 
             bundles.Add(new StyleBundle("~/Content/css")
                             .Include("~/Content/bootstrap.css",
-                                     "~/Content/site.css"));
+                                     "~/Content/site.css",
+                                     "~/Content/font-awesome.min.css"));
         }
 
     }
