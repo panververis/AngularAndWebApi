e83283ffb618e314b38d0ff0745e541c168f85ac
panververis <panververis@gmail.com>
2017-05-27 11:50:07 +0300

    Tweeked a bit the view. Added a new Getter-only field to the Vehicles DTO. Added project's Resources file

--- AngularAndWebApi/Models/DTOs/VehicleDTO.cs
+++ AngularAndWebApi/Models/DTOs/VehicleDTO.cs
@@ -4,10 +4,25 @@ namespace AngularAndWebApi.Models.DTOs
 {
     public class VehicleDTO
     {
+
+        //// Fields
         public int      ID              { get; set; }
         public string   Model           { get; set; }
         public Int16    MakeYear        { get; set; }
         public string   ChassisNumber   { get; set; }
         public Int16    EngineCapacity  { get; set; }
+
+        ////// Getter only fields
+        /// <summary>
+        /// Public object getter-only field returning the specific DTO's "Engine Capacity" property
+        /// in a user - friendly manner
+        /// </summary>
+        public string EngineCapacityDisplayText {
+            get {
+                string engineCapacityString = Convert.ToString(this.EngineCapacity);
+                return $"{engineCapacityString} {Resources.sCC}";
+            }
+        }
+
     }
 }
